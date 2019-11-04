30546b0a864c180d75dfefc84cb5323e11a487ef
Dwigth <dwigth32@outlook.es>
2019-11-04 17:53:55 -0600

    #Bug: Sobreposición de helper de un modulo a otro

--- app/helpers/hbs/home/general.ts
+++ app/helpers/hbs/home/general.ts
@@ -20,33 +20,6 @@ export const GeneralHelperManager: IHelperModel[] = [
    * =============================================
    */
   {
-    name: 'display_keys',
-    function: (keys: IKeys[]) => {
-      let result = '';
-      let length = keys.length;
-      for (let i = 0; i < length; i++) {
-        const K = keys[i];
-        result += `
-                <tr>
-                    <th>
-                      <label class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" name="" value="">
-                        <div class="custom-control-label"></div>
-                      </label>
-                    </th>
-                    <td>${K.nombre}</td>
-                    <td class="d-none d-sm-table-cell">${K.fecha}</td>
-                    <td class="d-none d-md-table-cell">${K.cantidad}</td>
-                    <td class="d-none d-md-table-cell">
-                    <button class="btn btn-secondary hide"> Mostrar </button>
-                    </td>
-                  </tr>
-                  `;
-      }
-      return new hbs.handlebars.SafeString(result);
-    }
-  },
-  {
     name: 'show_userdata',
     function: () => {
       const script = `
--- dist/app/helpers/hbs/home/general.js
+++ dist/app/helpers/hbs/home/general.js
@@ -22,18 +22,6 @@ exports.GeneralHelperManager = [
      * =============================================
      */
     {
-        name: 'display_keys',
-        function: function (keys) {
-            var result = '';
-            var length = keys.length;
-            for (var i = 0; i < length; i++) {
-                var K = keys[i];
-                result += "\n                <tr>\n                    <th>\n                      <label class=\"custom-control custom-checkbox\">\n                        <input type=\"checkbox\" class=\"custom-control-input\" name=\"\" value=\"\">\n                        <div class=\"custom-control-label\"></div>\n                      </label>\n                    </th>\n                    <td>" + K.nombre + "</td>\n                    <td class=\"d-none d-sm-table-cell\">" + K.fecha + "</td>\n                    <td class=\"d-none d-md-table-cell\">" + K.cantidad + "</td>\n                    <td class=\"d-none d-md-table-cell\">\n                    <button class=\"btn btn-secondary hide\"> Mostrar </button>\n                    </td>\n                  </tr>\n                  ";
-            }
-            return new hbs_1.default.handlebars.SafeString(result);
-        }
-    },
-    {
         name: 'show_userdata',
         function: function () {
             var script = "\n      <script>\n      if(localStorage.getItem('user') != null){\n        const avatar = document.getElementById('avatar');\n        const nombre = document.getElementById('nombre');\n        const nivel = document.getElementById('nivel');\n\n        const usuario = JSON.parse(localStorage.getItem('user'));\n        avatar.style.backgroundImage = 'url('+usuario.img+')';\n        nombre.textContent = usuario.nombre;\n        nivel.textContent = usuario.usuario;\n      }\n      </script>\n      ";
